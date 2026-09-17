d0c2ae88ab9ca7cdc9d6f8d0cd1c7c55ae978758
rioslara1 <albeiroriosa@gmail.com>
2026-09-17 16:15:44 -0500

    Modify app to use dynamic port and improve formatting
    Updated the app to run on a dynamic port and fixed minor formatting issues.

--- app.py
+++ app.py
@@ -88,4 +88,5 @@ def enviar_mensaje_texto(destinatario, texto):
     requests.post(url, headers=headers, json=payload)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
